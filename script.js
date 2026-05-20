55f9a6b3bf692aa1b6d2ca59df8353422a2f3240
Deekshitha Pedasingu <pedasingudeekshitha0@gmail.com>
2026-05-20 08:40:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showWeather(){
+
+document.getElementById("city").innerHTML="Hyderabad";
+
+document.getElementById("temp").innerHTML="32°C";
+
+document.getElementById("condition").innerHTML="Sunny";
+
+}
